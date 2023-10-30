22df0cfb9e9e8ad751eae473afb4b009539f920a
Jared E <jaredeic@yahoo.com>
2023-10-30 16:31:35 -0700

    added to javascript code and began debugging issue with leaderboard and scores not appearing after quiz ends

--- script.js
+++ script.js
@@ -6,30 +6,65 @@ document.addEventListener('DOMContentLoaded', function() {
     var scoresList = document.getElementById('scores');
     var scoreForm = document.getElementById('score-form');
     var initialsInput = document.getElementById('initials');
+    var startButton = document.getElementById('start-quiz');
 
     var timer = 60;
-    var currentQuesion = 1;
+    var currentQuestion = 1;
     var interval;
+    var correctAnswers = 0;
 
     function startTimer() {
         interval = setInterval(function() {
             timer--;
             timerEl.textContent = timer;
-            if (timer <= 0 || currentQuesion > 5) {
+            if (currentQuestion > 5 || timer <= 0) {
                 endQuiz();
             }
         }, 1000);
     }
 
     function showNextQuestion() {
-        var currentQuestionDiv = document.getElementById('question' + currentQuesion);
+        var currentQuestionDiv = document.getElementById('question' + currentQuestion);
         currentQuestionDiv.style.display = 'none';
-        currentQuesion++;
-        if (currentQuesion <= 5) {
-            var nextQuestionDiv = document.getElementById('question' + currentQuesion);
+        currentQuestion++;
+        if (currentQuestion <= 5) {
+            var nextQuestionDiv = document.getElementById('question' + currentQuestion);
             nextQuestionDiv.style.display = 'block';
         } else {
             endQuiz();
         }
     }
-})
+
+    function endQuiz() {
+        clearInterval(interval);
+        quizContainer.style.display = 'none';
+        leaderboard.style.display = 'block';
+    }
+
+    function submitScore(event) {
+        event.preventDefault();
+        var initials = initialsInput.value;
+        var scoreData = JSON.parse(localStorage.getItem('scores')) || [];
+        scoreData.push({ initials: initials, score: 60 - timer });
+        localStorage.setItem('scores', JSON.stringify(scoreData));
+        for (var i = 0; i < scoreData.length; i++) {
+            var li = document.createElement('li');
+            li.textContent = scoreData[i].initials + ' - ' + scoreData[i].score + ' seconds ';
+            scoresList.appendChild(li);
+        }
+    }
+
+    startButton.addEventListener('click', function() {
+        startButton.style.display = 'none';
+        quizContainer.style.display = 'block';
+        document.getElementById('question1').style.display = 'block';
+        startTimer();
+    });
+
+    scoreForm.addEventListener('submit', submitScore);
+    quizQuestions.addEventListener('click', function(event) {
+        if (event.target.classList.contains('next-btn')) {
+            showNextQuestion();
+        }
+    });
+});
